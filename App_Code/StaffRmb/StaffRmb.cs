a29cd372094ca57baaae1b5773ccf1f24f5865e7
brentn <brent.nesbitt@p2c.com>
2015-09-24 13:54:57 -0700

    add missing ampersand

--- App_Code/StaffRmb/StaffRmb.cs
+++ App_Code/StaffRmb/StaffRmb.cs
@@ -358,7 +358,7 @@ namespace StaffRmb
         static private async Task<string[]> staffWithSigningAuthorityAsync(string account, Decimal amount, string purpose)
         // Returns a list of staff with signing authority for a certain amount or greater on a given account
         {
-            string postData = string.Format("account={0}&amount={1}&exclude_administrators={2}log={3}", account, amount, "true", "Reimbursements:"+purpose);
+            string postData = string.Format("account={0}&amount={1}&exclude_administrators={2}&log={3}", account, amount, "true", "Reimbursements:"+purpose);
             string url = "https://staffapps.powertochange.org/AuthManager/webservice/get_signatories";
             string result = await getResultFromWebServiceAsync(url, postData);
             if (result.Length == 0 || result.Equals(WEB_SERVICE_ERROR))
