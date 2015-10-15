070b03a6551eda74d505736fc4f3851b67da62d1
brentn <brent.nesbitt@p2c.com>
2015-10-15 15:06:08 -0700

    add missing new controls

---
+++ DesktopModules/AgapeConnect/StaffRmb/Controls/CIDAAirfare.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="CIDAAirfare.ascx.cs" Inherits="ControlBase" %>
+
---
+++ DesktopModules/AgapeConnect/StaffRmb/Controls/CIDAAirfare.ascx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ControlBase : StaffRmb.StaffRmbControl {
+    new protected void Page_Init(object sender, EventArgs e)
+    {
+        base.Page_Init(sender, e);
+        show_mileage_fields();
+    }
+
+    private void show_mileage_fields()
+    {
+        lblOrigin.Visible = true;
+        lbOrigin.Visible = true;
+        tbOrigin.Visible = true;
+        tbDestination.Visible = true;
+    }
+
+    new public string Spare4
+    {
+        get { return tbOrigin.Text; }
+        set { tbOrigin.Text = value; }
+    }
+    new public string Spare5
+    {
+        get { return tbDestination.Text; }
+        set { tbDestination.Text = value; }
+    }
+
+    new public bool ValidateForm(int UserId)
+    {
+        if (!validate_required_fields()) return false;
+        if (!validate_description()) return false;
+        if (!validate_date()) return false;
+        if (!validate_amount()) return false;
+        if (!validate_receipt()) return false;
+        ErrorLbl.Text = "";
+        return true;
+    }
+    new public bool validate_required_fields()
+    {
+        TextBox[] required_fields = new TextBox[] { tbSupplier, tbOrigin, tbDestination, tbDesc, tbAmount };
+        bool result = true;
+        foreach (TextBox control in required_fields)
+        {
+            control.CssClass = control.CssClass.Replace("missing", "");
+            if (((TextBox)control).Text.Length == 0)
+            {
+                result = false;
+                control.CssClass = control.CssClass + " missing";
+            }
+        }
+        if (!result) ErrorLbl.Text = DotNetNuke.Services.Localization.Localization.GetString("Error.RequiredField", LocalResourceFile);
+        return result;
+    }
+
+}
+
