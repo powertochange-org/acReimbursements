dc92533cc768a39b7f5ba1b885661267c6b0f8de
brentn <brent.nesbitt@p2c.com>
2015-12-23 16:23:29 -0800

    add missing files

---
+++ App_Code/StaffRmb/IAdvanceView.cs
@@ -0,0 +1,38 @@
+﻿using DotNetNuke.Web.Mvp;
+using StaffRmb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PowerToChange.Modules.StaffRmb.Presenters
+{
+    public interface IAdvanceView : IModuleView<AP_Staff_RmbLine>
+    {
+        IEnumerable<OutstandingAdvance> OutstandingAdvances { set; }
+        StaffRmbDataContext DataContext { get; }
+        
+
+        event EventHandler<ModuleLoadEventArgs> ModuleLoad;
+
+    }
+
+
+    public class ModuleLoadEventArgs : EventArgs
+    {
+        public bool isPostBack { get; set; }
+    }
+
+    public class OutstandingAdvance
+    {
+        public long LineNo { get; set; }
+        public int RID {get; set;}
+        public int userId { get; set; }
+        public int status { get; set; }
+        public DateTime? date { get; set; }
+        public decimal originalAmount { get; set; }
+        public string outstandingAmount { get; set; }
+        public string comment { get; set; }
+    }
+
+}
